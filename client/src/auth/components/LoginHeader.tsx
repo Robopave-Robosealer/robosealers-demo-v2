9cfb2f0d15d1d43449d23851cbd0df398dffdf36
Mahmoud Nagy <mahmoud.nagyabosrea@gmail.com>
2024-06-01 10:22:30 +0300

    test change in login header for automatic deploy

--- client/src/auth/components/LoginHeader.tsx
+++ client/src/auth/components/LoginHeader.tsx
@@ -12,14 +12,13 @@ export default function LoginHeader() {
     <>
       <Flex direction="column" w={mobile ? "100%" : "48%"}>
         <Title order={1} fz={mobile ? "3rem" : "4rem"} c="dark.7">
-          Control your
+          Control your newwwwww
         </Title>
         <Title fz={mobile ? "3rem" : "4rem"} order={1} c={color}>
           Robosealers
         </Title>
         <Text c="dark.7" fz={mobile ? "1.3rem" : "1.7rem"}>
-          Monitoring the data and behaviors of your Robosealers. !! Test
-          continous deployment !!
+          Monitoring the data and behaviors of your Robosealers.
         </Text>
         <LoginForm />
       </Flex>
