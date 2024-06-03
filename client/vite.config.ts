f7bc14aefca1ef52ee9cc22b28d51f0148f9eb4c
Mahmoud Nagy <mahmoud.nagyabosrea@gmail.com>
2024-06-03 15:29:37 +0300

    ROB-153 change vite config: back to first default version

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     open: true
-  },
-  build: {
-    outDir: 'build',
-    sourcemap: false, // Disable sourcemaps for production
-    rollupOptions: {
-      output: {
-        // Optional: Customize the output settings if needed
-      }
-    }
   }
 })
