a67cdebf01a0a483ac727ea6cd17e9d425ae6a50
Mahmoud Nagy <mahmoud.nagyabosrea@gmail.com>
2024-06-03 15:19:39 +0300

    ROB-153 change vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,7 +8,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'build',
-    sourcemap: true, // Enable sourcemaps for better debugging
+    sourcemap: false, // Disable sourcemaps for production
     rollupOptions: {
       output: {
         // Optional: Customize the output settings if needed
