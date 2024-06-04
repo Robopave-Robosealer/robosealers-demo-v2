79a1c37a47099bbd39456b4b30d882495dd3f1b0
Mahmoud Nagy <mahmoud.nagyabosrea@gmail.com>
2024-06-04 13:52:05 +0300

    ROB-153 delete scripts directory as now we only use github actions deploy.yml script

--- scripts/install_dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/apps/robosealers-demo-v2/server
-npm install
--- scripts/start_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pm2 stop all
-pm2 start /home/ubuntu/apps/robosealers-demo-v2/server/configs/server.js --name robosealers # Adjust the start command as needed
